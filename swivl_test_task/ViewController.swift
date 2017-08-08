077f13c73cf4cdecb4c96aaf377871a7dfae055d
Admin <admin@Mac-Admin.local>
2017-08-08 22:06:01 +0300

    Initial Commit

---
+++ swivl_test_task/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swivl_test_task
+//
+//  Created by Admin on 08.08.17.
+//  Copyright © 2017 FC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
